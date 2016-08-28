288f1de7442883470382ad93b964605d953f9329
Daniel Hoffman <daniel@hoff.id.au>
2016-08-28 19:45:24 +1000

    change

--- esp8266/scripts/inisetup.py
+++ esp8266/scripts/inisetup.py
@@ -40,11 +40,8 @@ def setup():
     with open("/boot.py", "w") as f:
         f.write("""\
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-import gc
-#import webrepl
-#webrepl.start()
+import os, esp, network, socket, gc, webrepl
+webrepl.start()
 gc.collect()
 print "boo"
 print "boo"
